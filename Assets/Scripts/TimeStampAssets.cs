3d7cf4dd0527208b36e2ffa56639f882fbf7d944
Nicholas Costello <nicholasjcostello@gmail.com>
2017-03-24 18:16:50 -0400

    Deviated from objective

---
+++ Assets/Scripts/TimeStampAssets.cs
@@ -0,0 +1,14 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public class TimeStampAssets  
+{
+
+	[MenuItem("Assets/TimeStampAssetBundles")]
+	public static void TimeStampAssetBundles()
+	{
+
+	}
+
+}
