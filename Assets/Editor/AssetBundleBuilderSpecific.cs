234b23608a7f7a7736914f1e3f2bc63b0e62fee2
CostelloNicho <nicholasjcostello@gmail.com>
2017-04-01 22:17:40 -0400

    Cleanup

---
+++ Assets/Editor/AssetBundleBuilderSpecific.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.Linq;
+using System;
+using System.IO;
+
+public class AssetBundleBuilderSpecific : EditorWindow
+{
+
+    UnityEngine.Object[] objects;
+
+    // Add menu named "My Window" to the Window menu
+    [MenuItem("Assets/Build Specific Asset Bundles")]
+    static void Init()
+    {
+        // Get existing open window or if none, make a new one:
+        AssetBundleBuilderSpecific window = 
+            (AssetBundleBuilderSpecific)GetWindow(typeof(AssetBundleBuilderSpecific));
+        window.Show();
+    }
+
+    public AssetBundleBuilderSpecific() : base()
+    {
+        Selection.selectionChanged = OnSelectionChanged;
+    }
+
+    void OnGUI()
+    {
+        if(objects != null)
+        {
+            objects.ToList().ForEach(obj => { AddCell(obj); });
+        }
+    }
+
+    void OnSelectionChanged()
+    {
+        objects = Selection.objects;
+    }
+
+    void AddCell(UnityEngine.Object obj)
+    {
+        var name = obj.name;
+        EditorGUILayout.LabelField(name);
+    }
+
+    void Footer()
+    {
+        GUI.BeginGroup(new Rect(0, position.height - 30, position.width, 30));
+        if(GUILayout.Button("Build Asset Bundle"))
+        {
+            BuildAssetBundle();
+        }
+        GUI.EndGroup();
+    }
+
+    private void BuildAssetBundle()
+    {
+        string[] names = objects.Select(obj => obj.name).ToArray();
+      
+        //BuildPipeline.BuildAssetBundleExplicitAssetNames(objects, names, Path.Combine(Application.dataPath, "AssetBundles/Platforms/Standalone"));
+    }
+}
---
+++ Assets/Scripts/LoadFileAsyncExample.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using System.Linq;
+
+public class LoadFileAsyncExample : MonoBehaviour
+{
+
+    IEnumerator Start()
+    {
+        var bundleLoadRequest = AssetBundle.LoadFromFileAsync(Path.Combine(Application.streamingAssetsPath, "dailyworkout"));
+        yield return bundleLoadRequest;
+
+        var myLoadedAssetBundle = bundleLoadRequest.assetBundle;
+        if (myLoadedAssetBundle == null)
+        {
+            Debug.Log("Failed to load AssetBundle!");
+            yield break;
+        }
+
+        var allAssets = myLoadedAssetBundle.LoadAllAssetsAsync();
+        yield return allAssets;
+
+        allAssets.allAssets.ToList().ForEach(asset => { Debug.Log(asset.name); });
+    }
+}
